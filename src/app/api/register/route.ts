35f8dfcfbc9bf54c9d0c9c718861df35360fe9d1
EmJhey <emjheypb@gmail.com>
2024-07-08 19:40:27 -0400

    FIX: failed parse frame metatags

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -2,17 +2,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const searchParams = new URLSearchParams({
-  title: "Valid Email Required",
-});
 
 const errorResponse = new NextResponse(
   `<!DOCTYPE html><html>
     <head>
     <title>Error Registering</title>
     <meta property="fc:frame" content="vNext"/>
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/og?${searchParams}"/>
-    <meta property="fc:frame:image:aspect_ratio" content="1:1"/>
+    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/og"/>
     <meta property="fc:frame:input:text" content="EMAIL"/>
     <meta property="fc:frame:button:1" content="TRY AGAIN"/>
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/register"/>
@@ -25,7 +21,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     const request = await req.json();
     const email = request.untrustedData.inputText;
 
-    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+    if (!email || !/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
       return errorResponse;
     }
 
