de508808295b39eacd06d129c98b6d33045746e0
EmJhey <emjheypb@gmail.com>
2024-07-09 00:29:32 -0400

    FIX: post is not valid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,21 +18,6 @@ export const metadata: Metadata = {
   },
 };
 
-export const POST = async (): Promise<NextResponse> => {
-  return new NextResponse(
-    `<!DOCTYPE html><html>
-      <head>
-      <title>Error Registering</title>
-      <meta property="fc:frame" content="vNext"/>
-      <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/orc_red.png"/>
-      <meta property="fc:frame:image:aspect_ratio" content="1:1"/>
-      <meta property="fc:frame:button:1" content="START"/>
-      <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1"/>
-      </head>
-      </html>`
-  );
-};
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
