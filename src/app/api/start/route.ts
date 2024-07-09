444c1583d83e821b115ec0dd177e8be02b8a319a
EmJhey <emjheypb@gmail.com>
2024-07-09 13:15:32 -0400

    STYLE: reposition buttons

--- src/app/api/start/route.ts
+++ src/app/api/start/route.ts
@@ -8,10 +8,10 @@ export const POST = async (): Promise<NextResponse> => {
         <meta property="fc:frame" content="vNext"/>
         <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/orc_red.png"/>
         <meta property="fc:frame:image:aspect_ratio" content="1:1"/>
-        <meta property="fc:frame:button:1" content="REGISTER"/>
-        <meta property="fc:frame:button:1:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1"/>
-        <meta property="fc:frame:button:2" content="PLAY ROCK, PAPER, SCISSORS"/>
-        <meta property="fc:frame:button:2:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/under-construction"/>
+        <meta property="fc:frame:button:2" content="REGISTER"/>
+        <meta property="fc:frame:button:2:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1"/>
+        <meta property="fc:frame:button:1" content="PLAY ROCK, PAPER, SCISSORS"/>
+        <meta property="fc:frame:button:1:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/under-construction"/>
         </head>
         </html>`
   );
