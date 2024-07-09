fc63f6ff9014eec01ba2ece0e8ea2d876ad20204
EmJhey <emjheypb@gmail.com>
2024-07-09 11:44:08 -0400

    STYLE: revert

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -30,8 +30,10 @@ export async function GET(request: Request) {
             alignItems: "center",
             justifyContent: "center",
             flexDirection: "column",
-            textAlign: "center",
             backgroundImage: "linear-gradient(to bottom, #dbf4ff, #fff1f1)",
+            fontSize: 80,
+            fontWeight: 700,
+            textAlign: "center",
           }}>
           <p
             style={{
@@ -39,6 +41,8 @@ export async function GET(request: Request) {
                 "linear-gradient(90deg, rgb(0, 124, 240), rgb(0, 223, 216))",
               backgroundClip: "text",
               color: "transparent",
+              fontSize: 80,
+              fontWeight: 700,
               margin: 0,
             }}>
             {title}
@@ -50,6 +54,8 @@ export async function GET(request: Request) {
                   "linear-gradient(90deg, rgb(121, 40, 202), rgb(255, 0, 128))",
                 backgroundClip: "text",
                 color: "transparent",
+                fontSize: 80,
+                fontWeight: 700,
                 margin: 0,
                 marginTop: 20,
               }}>
